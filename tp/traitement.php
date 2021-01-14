b12d0d43acaad202d20166838027ec57577f03a4
Arnaudringeval <arnaud.ringeval@yahoo.fr>
2021-01-14 11:53:43 +0100

    tp

---
+++ tp/traitement.php
@@ -0,0 +1,86 @@
+<?php
+
+
+function translate($word, $direction)
+{
+    
+    $dictionary =
+    [
+        'cat'    => 'chat',
+        'dog'    => 'chien',
+        'oiseau' => 'bird',
+        'rabit' => 'lapin',
+        'cow' => 'vache',
+        'pig' => 'cochon',
+        'sheep' => 'mouton',
+        'chiken' => 'poulet'
+    ];
+
+
+    
+    switch($direction)
+    {
+        case 'toFrench':
+        
+        if(array_key_exists($word, $dictionary) == true)
+        {
+            
+            $translatedWord = $dictionary[$word];
+
+            $message = "Le mot '$word' se traduit par '$translatedWord'.";
+        }
+        else
+        {
+            
+            $message = "Je ne connais pas le mot '$word'.";
+        }
+        break;
+
+        case 'toEnglish':
+        
+        if(in_array($word, $dictionary) == true)
+        {
+            
+            $translatedWord = array_search($word, $dictionary);
+
+            $message = "Le mot '$word' se traduit par '$translatedWord'.";
+        }
+        else
+        {
+            
+            $message = "Je ne connais pas le mot '$word'.";
+        }
+        break;
+
+        default:
+        $message = "Je ne sais traduire qu'en français et en anglais !";
+    }
+
+    return $message;
+}
+
+
+
+$result = null;
+
+
+
+$direction = 'toFrench';
+if(array_key_exists('direction', $_POST) == true)
+{
+    
+    $direction = $_POST['direction'];
+}
+
+if(array_key_exists('word', $_POST) == true)
+{
+    
+    $word = strtolower($_POST['word']);
+
+    
+    $result = translate($word, $direction);
+}
+
+
+
+include 'index.phtml';
